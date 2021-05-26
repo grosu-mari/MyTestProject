b6dfed34c5748fea2b88c7cf4131dd237ba70063
grosu-mari <grosu.mari@mail.ru>
2021-05-26 15:12:58 +0300

    Create SecondViewController.

---
+++ MyTestProject/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  MyTestProject
+//
+//  Created by Mac on 26/05/2021.
+//
+
+import Foundation
+import UIKit
+
+final class SeocndViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let subview = UIView()
+        view.addSubview(subview)
+    }
+}
